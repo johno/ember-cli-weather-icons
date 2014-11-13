d8817da671aa8ab25e5e0ac8ca698009cec1802e
John Otander <johnotander@gmail.com>
2014-11-13 16:58:14 -0700

    Ensure the blueprint is properly formatted.

--- blueprints/ember-cli-weather-icons/index.js
+++ blueprints/ember-cli-weather-icons/index.js
@@ -1,5 +1,7 @@
+'use strict';
+
 module.exports = {
-  description: 'ember-cli-weather-icons'
+  description: 'ember-cli-weather-icons',
 
   afterInstall: function(options) {
     return this.addBowerPackageToProject('weather-icons');
