409feecf14a5cc0c4ddb4a1dd9173092b48cae1a
John Otander <johnotander@gmail.com>
2015-03-14 10:48:53 -0600

    Add a few more tests

--- tests/unit/helpers/weather-icon-test.js
+++ tests/unit/helpers/weather-icon-test.js
@@ -4,8 +4,12 @@ import {
 
 module('WeatherIconHelper');
 
-// Replace this with your real tests.
-test('it works', function() {
+test('it returns an icon with the correct sunny class', function() {
   var result = weatherIcon('sunny');
   equal(result.toString(), '<i class="wi wi-sunny"></i>');
+});
+
+test('it returns an icon with the correct moon-full class', function() {
+  var result = weatherIcon('moon-full');
+  equal(result.toString(), '<i class="wi wi-moon-full"></i>');
 });
