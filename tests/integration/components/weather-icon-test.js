9aa4d07a3a444fcd20b34ad76d368a9f829674cb
Alexey Kulakov <uni_que@me.com>
2018-10-08 03:57:18 -0700

    updating ember-cli and upgrading tests

--- tests/integration/components/weather-icon-test.js
+++ tests/integration/components/weather-icon-test.js
@@ -1,50 +1,50 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { find } from '@ember/test-helpers';
+import { set } from '@ember/object';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('weather-icon', 'Integration | Component | weather icon', {
-  integration: true
-});
+module('Integration | Component | weather icon', function(hooks){
+  setupRenderingTest(hooks),
 
-test('it renders', function(assert) {
-  
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });" + EOL + EOL +
+  test('it renders', async function(assert) {
 
-  this.render(hbs`{{weather-icon}}`);
+    await render(hbs`{{weather-icon}}`);
 
-  assert.equal(this.$().text().trim(), '');
+    assert.equal(find('*').textContent.trim(), '');
 
-  // Template block usage:" + EOL +
-  this.render(hbs`
-    {{#weather-icon}}
-      template block text
-    {{/weather-icon}}
-  `);
+    await render(hbs`
+      {{#weather-icon}}
+        template block text
+      {{/weather-icon}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
-});
+    assert.equal(find('*').textContent.trim(), 'template block text');
+  });
 
-test('it uses the correct tag', function(assert) {
-  this.render(hbs`{{weather-icon}}`);
-  assert.equal(this.$('> :first-child').prop('tagName'), 'I');
-});
+  test('it uses the correct tag', async function(assert) {
+    await render(hbs`{{weather-icon}}`);
+    assert.ok(find('i'));
+  });
 
-test('it adds the wi class', function(assert) {
-  this.render(hbs`{{weather-icon name='sunny'}}`);
-  assert.ok(this.$('> :first-child').hasClass('wi'));
-});
+  test('it adds the wi class', async(assert) => {
+    await render(hbs`{{weather-icon name='sunny'}}`);
+    assert.ok(find('i').classList.contains('wi'));
+  });
 
-test('it adds the prefixed class', function(assert) {
-  this.render(hbs`{{weather-icon name='day-sunny'}}`);
-  assert.ok(this.$('> :first-child').hasClass('wi-day-sunny'));
-});
+  test('it adds the prefixed class', async function(assert) {
+    await render(hbs`{{weather-icon name='day-sunny'}}`);
+    assert.ok(find('i').classList.contains('wi-day-sunny'));
+  });
 
-test('it correctly binds prefixedClass to the name property', function(assert) {
-  this.set('currWeather', 'day-sunny');
+  test('it correctly binds prefixedClass to the name property', async function(assert) {
+    set(this, 'currWeather', 'day-sunny');
+    await render(hbs`{{weather-icon name=currWeather}}`);
+    assert.ok(find('i').classList.contains('wi-day-sunny'));
 
-  this.render(hbs`{{weather-icon name=currWeather}}`);
-
-  this.set('currWeather', 'cloudy');
-
-  assert.ok(this.$('> :first-child').hasClass('wi-cloudy'));
+    set(this, 'currWeather', 'cloudy');
+    await render(hbs`{{weather-icon name=currWeather}}`);
+    assert.ok(find('i').classList.contains('wi-cloudy'));
+  });
 });
